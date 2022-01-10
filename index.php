05b8c02700d161eecd4bad5d8a919048de73f71c
coque <coquelet.samuel@gmail.com>
2022-01-10 20:34:17 +0100

    fini

--- index.php
+++ index.php
@@ -1,35 +1,39 @@
 <?php
-
-// Premiere ligne
-
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana", "Orange");
+$arrlength = count($fruits);
+for($i = 0; $i < $arrlength; $i++) {
+    echo $fruits[$i]."<br>";
+}
+echo $fruits[1]."<br>";
 
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is ". $age["Ben"]." years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y ;
 }
 
-
+echo "<br>";
 
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+sort($colors);
+print_r($colors);
+echo "<br>";
+rsort($colors);
+print_r($colors);
+echo "<br>";
 
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
-
-
-
-
-
+asort($age);
+foreach ($age as $key => $val){
+    echo "$key : $val <br>";
+}
